2105f7f3cfaea073ca1d1a7a4e5f13c032625c14
Natali <natlyly3@mail.ru>
2023-02-27 20:07:27 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿int GetNumber(string message)
+{
+    int resalt = 0;
+    while(true)
+    {
+        Console.WriteLine(message);
+        if (int.TryParse(Console.ReadLine(), out resalt))
+        {
+            break;
+        }
+        else
+        {
+            Console.WriteLine("Ввели не число. Повторите ввод");
+        }
+    }
+    return resalt;
+}
+
+int AkkermanFunction(int m, int n)
+{
+    if (m == 0) 
+    {
+    return n + 1;
+    }
+    else if (n == 0 && m> 0)
+        {
+        return AkkermanFunction(m - 1, 1);
+        }
+        else
+        {
+            return (AkkermanFunction(m - 1, AkkermanFunction(m, n-1)));
+        }
+ }
+int m = GetNumber("Введите M -> ");
+int n = GetNumber("Введите N -> ");
+Console.WriteLine(AkkermanFunction(m, n));
